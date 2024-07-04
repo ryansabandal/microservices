3fa7f3304604fb0a4947e3c600f396f58256a2f8
Ryan <rssabandal@globe.com.ph>
2024-07-04 15:26:51 +0800

    Updated Accounts entity

--- accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
+++ accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -1,33 +1,23 @@
 package com.eazybytes.accounts.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Getter
-@Setter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
-public class Accounts extends BaseEntity {
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Accounts extends  BaseEntity {
 
-  @Column(name = "customer_id")
-  @Id
-  private Long customerId;
+    @Column(name="customer_id")
+    private Long customerId;
 
-  @Column(name = "account_number")
-  @Id
-  private Long accountNumber;
+    @Column(name="account_number")
+    @Id
+    private Long accountNumber;
 
-  @Column(name="account_type")
-  private String accountType;
+    @Column(name="account_type")
+    private String accountType;
 
-  @Column(name="branch_address")
-  private String branchAddress;
+    @Column(name="branch_address")
+    private String branchAddress;
+
 }
