4b70498b748b5fbe2791ca94ccd1fa9dd3d59e92
Ryan <rssabandal@globe.com.ph>
2024-07-04 15:01:00 +0800

    Updated AccountsController

--- accounts/src/main/java/com/eazybytes/accounts/exceptions/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/eazybytes/accounts/exceptions/GlobalExceptionHandler.java
@@ -5,13 +5,15 @@ import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import com.eazybytes.accounts.dto.ErrorResponseDto;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+  
+  @ExceptionHandler(CustomerAlreadyExistsException.class)
   public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception,
       WebRequest webRequest) {
     ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
