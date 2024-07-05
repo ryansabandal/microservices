76d53fbe218dca0c67b70fa65d845b1d8f3e28bf
Ryan <rssabandal@globe.com.ph>
2024-07-05 14:26:46 +0800

    Added global exception handler

--- accounts/src/main/java/com/eazybytes/accounts/exceptions/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/eazybytes/accounts/exceptions/GlobalExceptionHandler.java
@@ -12,7 +12,17 @@ import com.eazybytes.accounts.dto.ErrorResponseDto;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-  
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+      WebRequest webRequest) {
+    ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+        webRequest.getDescription(false),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        exception.getMessage(), LocalDateTime.now());
+    return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
   @ExceptionHandler(CustomerAlreadyExistsException.class)
   public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception,
       WebRequest webRequest) {
